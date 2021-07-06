84779ecf299c9ce5fe316ad0b721ce6f2c20dbaa
Daksh Jain <jaindaksh58@gmail.com>
2021-07-07 03:21:50 +0530

    Create Segregate0_1_2.py

---
+++ Assignment1/Segregate0_1_2.py
@@ -0,0 +1,20 @@
+def Segregate0_1_2(li):
+    i = 0
+    j = 0
+    k = len(li) - 1
+    while(j <= k):
+        if li[j] == 0:
+            li[i], li[j] = li[j], li[i]
+            i += 1
+        elif li[j] == 1:
+            j += 1
+        else:
+            li[j], li[k] = li[k], li[j]
+            k -= 1
+    return li
+
+
+li = list(map(int, input().split()))
+li = Segregate0_1_2(li)
+for i in li:
+    print(i, end = " ")
